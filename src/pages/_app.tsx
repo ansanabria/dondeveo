63d14bc4b71b41dd487ac8d8229753ee3cfdcdae
Rafael Sanabria <rafael.d.sanabria@gmail.com>
2022-09-30 10:58:58 -0500

    Add Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import { MetadataProvider } from "@/context/MetadataContext"
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
+import Script from "next/script"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MetadataProvider>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="analytics" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <Component {...pageProps} />
     </MetadataProvider>
   )
